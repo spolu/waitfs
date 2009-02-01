210439eee64cf74b99cf1716e35b7ed9ef88359d
Ryan Stutsman <stutsman@scs.stanford.edu>
2009-02-01 15:01:22 -0800

    python wants statable entries in waitfs before it will allow me to make symlinks

--- tests/instascp/instascp.py
+++ tests/instascp/instascp.py
@@ -1,19 +1,85 @@
 import waitfs
+import sys
 import os
+import subprocess
+from threading import Thread, RLock
 
 fake = 'foo'
 replacement = '/bin/ls'
 
+DEBUG = True
+
+def _debug(s):
+	if DEBUG:
+		print 'DEBUG (%s): %s' % (__name__, repr(s))
+
+class host(object):
+	def __init__(self, name):
+		self.name = name
+
+	def _run(self, cmd):
+		full_cmd = ['ssh', self.name, cmd]
+		_debug(full_cmd)
+		p = subprocess.Popen(full_cmd, stdout=subprocess.PIPE)
+		output = p.communicate()[0]
+		return output
+
+	def listdir(self, path):
+		# TODO make sure path is safe
+		output = self._run('ls -1 "%s"' % path) 
+		l = output.split('\n')
+		return [os.path.join(path, e) for e in l][:-1]
+
+
+def instascp(hostname, remote, local):
+	"""hostname with abspath for remote and local dirs"""
+	h = host(hostname)		# remote host
+	c = waitfs.connection()		# waitfs daemon
+	m = {}				# map from local to remote paths
+	mlock = RLock()			# lock to protect m accesses
+
+	def do_dir(rdir):
+		for rpath in h.listdir(rdir):
+			# TODO this code is pretty fragile
+			# probably a number of path escaping vulns
+			lr = len(remote)
+			front, back = rpath[:lr], rpath[lr + 1:]
+			if front != remote:
+				raise Exception('bad remote path: %s' % rpath)
+			path = os.path.join(local, back)
+			try:
+				mlock.acquire()
+				m[path] = rpath
+			finally:
+				mlock.release()
+			print c
+			c.getlinkfor(path)
+
+	do_dir(remote)
+
+
+def usage():
+	print >> sys.stderr, '%s <remote_host> <remote_dir> <local_dir>' % sys.argv[0]
+	raise SystemExit()
+
+def cannonicalize_path(p):
+	return os.path.abspath(os.path.expandvars(os.path.expanduser(p)))
+
 def main():
-	c = waitfs.connection()
-	lid, path = c.getlink()
-	print 'Created waitfs target', lid, path
+	try:
+		hostname, remote, local = sys.argv[1:]
+		remote = cannonicalize_path(remote)
+		local = cannonicalize_path(local)
+	except:
+		usage()
+	
+	if not os.path.isdir(local):
+		print >> sys.stderr, '<local_dir> must be a dir'
+		usage()
 
-	os.symlink(path, fake)
+	_debug('instascp %s %s %s' % (hostname, remote, local))
+	instascp(hostname, remote, local)
 
-	# need some means to get callbacks from waitfs
 
-	c.setlink(lid, replacement)
-	print 'Access should now be redirected by waitfs'
 
 if __name__ == '__main__': main()
--- tests/instascp/waitfs.py
+++ tests/instascp/waitfs.py
@@ -1,4 +1,6 @@
 import socket
+from threading import Thread, RLock
+import os
 
 SOCK_PATH = '/var/waitfs'
 
@@ -12,7 +14,23 @@ DEBUG = True
 
 def _debug(s):
 	if DEBUG:
-		print 'DEBUG: %s' % repr(s)
+		print 'DEBUG (%s): %s' % (__name__, repr(s))
+
+def synchronized(func):
+	def wrapper(self,*__args,**__kw):
+		try:
+			rlock = self._sync_lock
+		except AttributeError:
+			rlock = self.__dict__.setdefault('_sync_lock',RLock())
+			rlock.acquire()
+		try:
+			return func(self,*__args,**__kw)
+		finally:
+			rlock.release()
+		wrapper.__name__ = func.__name__
+		wrapper.__dict__ = func.__dict__
+		wrapper.__doc__ = func.__doc__
+		return wrapper
 
 class error(Exception):
 	pass
@@ -22,9 +40,22 @@ class unexpected_response(Exception):
 
 class connection(object):
 	def __init__(self):
+		self.paths = {}
 		self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 		self.sock.connect(SOCK_PATH)
 
+	# TODO This seems to be broken, must fix or acquire locks manually
+	#@synchronized
+	def getlinkfor(self, path):
+		"""Porcelain for getlink.  Caller passes in path they want
+		a waitfs link for.  The connection will track the association
+		from lid -> (path, linkpath) for the user."""
+		lid, lpath = self.getlink()
+		self.paths[path] = (lid, lpath)
+		_debug('symlink %s %s' % (path, lpath))
+		os.symlink(path, lpath)
+
+	#@synchronized
 	def getlink(self):
 		request = '%s\n' % GETLINK_CMD
 		_debug('Request: %s' % request)
@@ -41,11 +72,22 @@ class connection(object):
 		elif results[0] != OK_CMD:
 			raise unexpected_response(response)
 
-		lid, path = results[1:]
+		lid, lpath = results[1:]
 		lid = int(lid)
 
-		return lid, path
+		return lid, lpath
 
+	@synchronized
+	def setlinkfor(self, path, contentpath):
+		"""Atomically moves contentpath to path and notifies the waitfs
+		daemon by performing setlink on the appropriate (lid, path)"""
+		self._sync_lock.acquire()
+		lid, lpath = self.paths[path]
+		os.rename(contentpath, path)
+		self.setlink(lid, path)
+		del self.paths[path]
+
+	@synchronized
 	def setlink(self, lid, path):
 		request = '%s %d %s' % (SETLINK_CMD, lid, path)
 		_debug('Request: %s' % request)
@@ -64,5 +106,17 @@ class connection(object):
 
 		return
 
+	def start_callback_monitor(self, cb):
+		self.cb = cb
+		l = lambda c: self._callback_monitor(cb)
+		self.monitor = Thread(target=l)
 
+	def _callback_monitor(self, cb):
+		while True:
+			time.sleep(.1)
+			try:
+				self._sync_lock.acquire()
+			finally:
+				self._sync_lock.release()
+		
 
