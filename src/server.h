0547e3046e61f3e8bc8ea327c05a52a6acd35869
Stanislas POLU <spolu@DNab422255.Stanford.EDU>
2009-02-06 16:02:05 -0800

    Implemented server callback sending

--- src/server.h
+++ src/server.h
@@ -1,9 +1,10 @@
 #ifndef _SERVER_H
 #define _SERVER_H
 
-#define OPEN_CMD "open"        // >> open uid          << ok            || << error
-#define GETLINK_CMD "getlink"  // >> getlink           << ok lid path   || << error
-#define SETLINK_CMD "setlink"  // >> setlink lid path  << ok            || << error
+#define OPEN_CMD "open"          // >> open uid           << ok                     || << error
+#define GETLINK_CMD "getlink"    // >> getlink            << readlink ok lid path   || << error
+#define SETLINK_CMD "setlink"    // >> setlink lid path   << setlink ok             || << error
+#define READLINK_CMD "readlink"  // << readlink lid path
 #define OK_CMD "ok"
 #define ERROR_CMD "error"
 
